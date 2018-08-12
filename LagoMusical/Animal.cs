874b4848823364b842c53f6770f4d5be6355ab1a
joseman1991 <josama.14@gmail.com>
2018-08-12 07:02:57 -0500

    Prueba Author

--- LagoMusical/Animal.cs
+++ LagoMusical/Animal.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/**
+ * 
+ * 
+ * @Author José Rosado
+ * 
+ * 
+ * 
+ */
 namespace LagoMusical
 {
    public class Animal
